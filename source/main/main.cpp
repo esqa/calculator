d4fe12a5ad8b819e36c9a2ec0b56fe5f08f9b306
Joshua <37853883+esqa@users.noreply.github.com>
2024-07-29 04:08:05 +0200

    calc again

--- source/main/main.cpp
+++ source/main/main.cpp
@@ -2,14 +2,19 @@
 #include <chrono>
 #include <climits>
 #include <iomanip>
+#include <Windows.h>
 
-int main() {
-    const int n = INT_MAX;
+int main()
+{
+    const int n = 1024;
     long long sum;
 
     auto calc_start = std::chrono::high_resolution_clock::now();
 
-    sum = static_cast<long long>(n) * (n - 1) / 2;
+    for (int i = 0; i < n; i++)
+    {
+        sum = i;
+    }
 
     auto calc_end = std::chrono::high_resolution_clock::now();
 
@@ -18,6 +23,11 @@ int main() {
     std::cout << std::fixed << std::setprecision(9);
     std::cout << "Sum calculation duration: " << calc_duration.count() << " seconds\n";
     std::cout << "Sum: " << sum << "\n";
+
+    SetConsoleTitleA("calc but fast"); // i'm calling this after the main calc since it's gonna mess with the speed
+
+    std::cout << "\nPress enter to exit";
+    std::cin.get();
 
     return 0;
 }
