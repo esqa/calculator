c8585ede7b84201a3b88c284d08e48b25292a953
Joshua <37853883+esqa@users.noreply.github.com>
2024-07-29 03:57:45 +0200

    calc but fast

--- source/main/main.cpp
+++ source/main/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
-#include "main.h"
+#include <chrono>
+#include <climits>
+#include <iomanip>
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+int main() {
+    const int n = INT_MAX;
+    long long sum;
+
+    auto calc_start = std::chrono::high_resolution_clock::now();
+
+    sum = static_cast<long long>(n) * (n - 1) / 2;
+
+    auto calc_end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> calc_duration = calc_end - calc_start;
+
+    std::cout << std::fixed << std::setprecision(9);
+    std::cout << "Sum calculation duration: " << calc_duration.count() << " seconds\n";
+    std::cout << "Sum: " << sum << "\n";
+
     return 0;
 }
